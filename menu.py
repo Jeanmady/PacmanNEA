b65e80a2105dbd0be52983d3b4fb4826a94811ad
Jeanmady <62425251+Jeanmady@users.noreply.github.com>
2022-03-16 09:49:56 +0000

    Update menu.py

--- menu.py
+++ menu.py
@@ -2,20 +2,25 @@ import pygame
 
 
 class Menu():
+    """ Base Menu Class inherited by other Menus """
+    """ Attributes """
     def __init__(self, game):
-        self.game = game # so access to games variables are accesible
-        self.mid_w, self.mid_h = self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2
-        self.run_display = True
-        self.cursor_rect_left = pygame.Rect(0,0,20,20) #20 by 20 square as a cursor later add image l
-        self.offset_left = -150 #moves cursor to the left
-        self.cursor_rect_right = pygame.Rect(0,0,20,20) #20 by 20 square as a cursor later add image r
-        self.offset_right = 150 #moves cursor to the right
+        self.game = game                                                                                   # Takes the game class attributes to make them accessible in the menu class
+        self.mid_w, self.mid_h = self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2                          # Cakcukates and stores centre values in the display
+        self.run_display = True                                                                            # Boolean variab;e used in method display_menu
+        self.cursor_rect_left = pygame.Rect(0,0,20,20)                                                     # 20 by 20 square as a cursor for left hand side
+        self.offset_left = -150                                                                            # moves cursor to the left
+        self.cursor_rect_right = pygame.Rect(0,0,20,20)                                                    # 20 by 20 square as a cursor for right hand side
+        self.offset_right = 150                                                                            # moves cursor to the right
 
+    """ Methods """
     def draw_cursors(self):
+        """ Draws cursors onto the screen """
         self.game.draw_text_8bit('*', 15, self.cursor_rect_left.x, self.cursor_rect_left.y)
         self.game.draw_text_8bit('*', 15, self.cursor_rect_right.x, self.cursor_rect_right.y)
 
     def blit_screen(self):
+        """ Resets and updates screen should be used in while loop to ensure constant renewal of screen """
         self.game.window.blit(self.game.display, (0,0)) 
         pygame.display.update()
         self.game.reset_keys()
@@ -24,9 +29,10 @@ class Menu():
 
 
 class LoginMenu(Menu):
+    """ Attributes """
     def __init__(self, game):
-        Menu.__init__(self, game)  # gets all variables from menu class
-        self.state = "Login"  # sets initial position of cursour to be login as its thew fisrt item
+        Menu.__init__(self, game)                                                                          # gets all variables from menu class
+        self.state = "Login"                                                                               # sets initial position of cursour to be login as its the fisrt item
         self.loginx, self.loginy = self.mid_w, self.mid_h + 10
         self.registerx, self.registery = self.mid_w, self.mid_h + 30
         self.exitx, self.exity = self.mid_w, self.mid_h + 50
@@ -34,6 +40,7 @@ class LoginMenu(Menu):
         self.cursor_rect_right.midtop = (self.loginx + self.offset_right, self.loginy)
 
     def display_menu(self):
+        """ Method to display Menu onto the screen """
         self.run_display = True
         while self.run_display:
             self.game.check_events()
@@ -71,6 +78,7 @@ class LoginMenu(Menu):
                 self.state = 'Login'
 
     def check_input(self):
+        """ Checks for user input on keyboard """
         self.move_cursors()
         if self.game.START_KEY:
             if self.state == 'Login':
@@ -81,7 +89,39 @@ class LoginMenu(Menu):
                 self.game.intro, self.game.running, self.game.playing = False, False, False
             self.run_display = False
 
+class SignInMenu(Menu):
+    """ Class to create Sign In sub Menu for Login Page """     
+    """ Attributes """
+    def __init__(self, game):
+        Menu.__init__(self, game)
+        self.state = "Enter Username"
+        self.usernamex, self.usernamey = self.mid_w, self.mid_h 
+        self.passwordx, self.passwordy = self.mid_w, self.mid_h + 20
+        self.loginx, self.loginy = self.mid_w, self.mid_h + 70
+        self.exitx, self.exity = self.mid_w, self.mid_h + 90
+        self.inp_username, self.inp_pass = '', ''
+        self.cursor_rect_left.midtop = (self.usernamex - 100 + self.offset_left, self.usernamey)
+        self.cursor_rect_right.midtop = (self.usernamex + 40 + self.offset_right, self.usernamey)
+
+    def display_menu(self):
+        """ Method to display Menu onto the screen """
+        self.run_display = True
+        while self.run_display:
+            self.game.check_events()
+            self.check_input()
+            self.game.display.fill(self.game.BLACK)
+            self.game.draw_text_8bit('Enter Username', 15, self.usernamex - 100, self.usernamey)
+            self.game.draw_text_8bit("Enter Password", 15, self.passwordx - 100, self.passwordy)
+            self.game.draw_text_8bit("Login", 20, self.signinx, self.signiny)
+            self.game.draw_text_8bit("Exit", 20, self.exitx, self.exity)
+            self.game.draw_text(self.inp_username, 10, self.usernamex + 40, self.usernamey )
+            self.game.draw_text(self.inp_pass, 10, self.passwordx + 40, self.passwordy)
+            self.draw_cursors()
+            self.blit_screen()
+
 class RegisterMenu(Menu):
+    """ Class to create Register Sub Menu for Login Menu Class """
+    """ Attributes """
     def __init__(self, game):
         Menu.__init__(self, game)
         self.state = "Enter Username"
@@ -95,6 +135,7 @@ class RegisterMenu(Menu):
         self.cursor_rect_right.midtop = (self.usernamex + 40 + self.offset_right, self.usernamey)
 
     def display_menu(self):
+        """ Method to display Menu onto the screen """
         self.run_display = True
         while self.run_display:
             self.game.check_events()
@@ -164,6 +205,7 @@ class RegisterMenu(Menu):
         
 
     def check_input(self):
+        """ Checks for user input on keyboard """
         self.move_cursors()
         if self.game.START_KEY:
             if self.state == 'Register':
@@ -176,6 +218,7 @@ class RegisterMenu(Menu):
         
 
 class MainMenu(Menu):
+    """ Attributes """
     def __init__(self, game):
         Menu.__init__(self, game) #gets all variables form menu class
         self.state = "Start" #so cursor points at start game 
@@ -188,6 +231,7 @@ class MainMenu(Menu):
         self.cursor_rect_right.midtop = (self.startx + self.offset_right, self.starty)
 
     def display_menu(self):
+        """ Method to display Menu onto the screen """
         self.run_display = True
         while self.run_display:
             self.game.check_events()
@@ -205,10 +249,12 @@ class MainMenu(Menu):
             self.blit_screen()
 
     def move_cursors(self):
-        """Method moves cursor by:
-            Checking state, 
-            adjusting cursors, 
-            then readjust state"""
+        """
+            Method moves cursor by:
+            Checking state
+            Adjusting cursors
+            Then re-adjust state
+        """
         if self.game.DOWN_KEY:
             if self.state == 'Start':
                 self.cursor_rect_left.midtop = (self.controlsx + self.offset_left, self.controlsy)
@@ -246,6 +292,7 @@ class MainMenu(Menu):
                 self.state = 'Start'
 
     def check_input(self):
+        """ Checks for user input on keyboard """
         self.move_cursors()
         if self.game.START_KEY:
             if self.state == 'Start':
@@ -261,6 +308,7 @@ class MainMenu(Menu):
             self.run_display = False
 
 class GameControls(Menu):
+    """ Attributes """
     def __init__(self, game):
         Menu.__init__(self, game)
     
